8431e5fd416a7011e473aa385b8152da023a1762
FunnyBoxification <pablorom93@gmail.com>
2016-09-27 23:22:14 -0300

    Creacion de algunas tablas.

--- script_inicial.sql
+++ script_inicial.sql
@@ -15,8 +15,6 @@ GO
 CREATE PROCEDURE SIEGFRIED.CREATE_TABLES_AND_FILL
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
 	CREATE TABLE SIEGFRIED.USUARIOS (
@@ -29,16 +27,106 @@ BEGIN
 		PRIMARY KEY(id_usuario)
 	);
 
+	CREATE TABLE SIEGFRIED.ROLES
+	(
+		Id_Rol						numeric(18,0) IDENTITY(1,1) NOT NULL,
+		Nombre						nvarchar(255),
+		Habilitado					numeric(18,0) DEFAULT 1,
+		PRIMARY KEY(Id_Rol)
+	);
+
+	CREATE TABLE SIEGFRIED.ROLES_USUARIOS 
+	(
+		Id_Rol						numeric(18,0),
+		Id_Usuario					numeric(18,0),
+		PRIMARY KEY(Id_Rol, Id_Usuario),
+		FOREIGN KEY(Id_Rol) REFERENCES SIEGFRIED.ROLES(Id_Rol),
+		FOREIGN KEY(Id_Usuario) REFERENCES SIEGFRIED.USUARIOS(Id_Usuario)
+
+	);
+
+
+	CREATE TABLE SIEGFRIED.FUNCIONALIDADES
+	(
+		Id_Funcionalidad			numeric(18,0) IDENTITY(1,1) NOT NULL,
+		Nombre						nvarchar(255),
+		PRIMARY KEY(Id_Funcionalidad)
+	);
+	
+	CREATE TABLE SIEGFRIED.FUNCIONALIDES_ROLES
+	(
+		Id_Funcionalidad			numeric(18,0),
+		Id_Rol						numeric(18,0),
+		PRIMARY KEY(Id_Funcionalidad,Id_Rol),
+		FOREIGN KEY(Id_Rol) REFERENCES SIEGFRIED.ROLES(Id_Rol),
+		FOREIGN KEY(Id_Funcionalidad) REFERENCES SIEGFRIED.FUNCIONALIDADES(Id_Funcionalidad)
+	);
+
+	CREATE TABLE SIEGFRIED.ESTADOS_CIVILES( 
+		id_estado numeric(18,0) identity(1,1) NOT NULL PRIMARY KEY,
+		descripcion nvarchar(255)
+	);
+
 	CREATE TABLE SIEGFRIED.AFILIADOS(
-		id_afiliado numeric(18,0) IDENTITY(1,1) NOT NULL,
+		id_afiliado numeric(18,0) NOT NULL,
 		nombre nvarchar(255),
 		direccion nvarchar(255),
 		telefono numeric(18,0),
 		mail nvarchar(255),
 		fecha_nacimiento datetime,
 		sexo numeric(18,0) DEFAULT NULL,
+		estado_civil numeric(18,0) FOREIGN KEY REFERENCES SIEGFRIED.ESTADOS_CIVILES(id_estado),
+		cantidad_familiares numeric(18,0) DEFAULT 0,
+		id_plan numeric(18,0) foreign key references SIEGFRIED.PLANES(id_plan)
+
 		PRIMARY KEY(id_afiliado)
 	);
+
+	CREATE TABLE SIEGFRIED.PLANES(
+		id_plan numeric(18,0) PRIMARY KEY,
+		descripcion varchar(255),
+		precio_bono_consulta numeric(18,0)
+	);
+
+	CREATE TABLE SIEGFRIED.PROFESIONALES ( 
+		id_profesional numeric(18,0) -- cuenta como matricula?
+	);
+
+	CREATE TABLE SIEGFRIED.ESPECIALIDADES (
+		id_especialidad numeric(18,0) NOT NULL PRIMARY KEY,
+		descripcion varchar(255),
+		id_tipo_especialidad numeric(18,0) foreign key references SIEGFRIED.TIPOS_ESPECIALIDADES(id_tipo)
+	);
+
+	CREATE TABLE SIEGFRIED.TIPOS_ESPECIALIDADES ( 
+		id_tipo numeric(18,0) primary key,
+		descripcion varchar(255)
+	);
+
+	CREATE TABLE SIEGFRIED.AGENDA (
+		id_profesional numeric(18,0) not null,
+		dia_semana numeric(18,0) not null,
+		hora_desde numeric(18,0) not null,
+		hora_hasta numeric(18,0) not null,
+		primary key(id_profesional,dia_semana),
+		foreign key(id_profesional) references SIEGFRIED.PROFESIONALES(id_profesional)
+	)
+
+	CREATE TABLE SIEGFRIED.TURNOS(
+		id_turno numeric(18,0) primary key,
+		id_afiliado numeric(18,0) foreign key references SIEGFRIED.AFILIADOS(id_afiliado),
+		id_profesional numeric(18,0) foreign key references SIEGFRIED.PROFESIONALES(id_profesional),
+		fecha datetime,
+		--numero_turno numeric(18,0) -- ????
+	)
+
+	CREATE TABLE SIEGFRIED.CONSULTAS()
+
+	CREATE TABLE SIEGFRIED.BONOS()
+
+	CREATE TABLE SIEGFRIED.CANCELACION()
+
+	CREATE TABLE SIEGFRIED.TIPOS_CANCELACION()
     
 END
 GO
