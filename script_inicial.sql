8170185a24fa51ef8c4833eaa8112fb129a62162
Julian Crocco <crocco.julian@gmail.com>
2016-09-29 00:27:27 -0300

    corregi el create tables y empece a normalizar. Tengo sueño sigo mañana

--- script_inicial.sql
+++ script_inicial.sql
@@ -19,13 +19,27 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
+	CREATE TABLE SIEGFRIED.ESTADOS_CIVILES( 
+		id_estado numeric(18,0) identity(1,1) NOT NULL PRIMARY KEY,
+		descripcion nvarchar(255)
+	);
+
 	CREATE TABLE SIEGFRIED.USUARIOS (
 		id_usuario numeric(18,0) IDENTITY(1,1) NOT NULL,
 		username NVARCHAR(255),
 		contrasenia binary(32),
 		habilitado				numeric(18,0) DEFAULT 1,
 		intentos_login			numeric(18,0) DEFAULT 0,
-		primera_vez				numeric(18,0),
+		nombre nvarchar(255),
+		apellido nvarchar(255),
+		direccion nvarchar(255),
+		tipo_dni nvarchar(255),
+		nro_dni numeric(18,0),
+		telefono numeric(18,0),
+		mail nvarchar(255),
+		fecha_nacimiento datetime,
+		sexo numeric(18,0) DEFAULT NULL,
+		
 		PRIMARY KEY(id_usuario)
 	);
 
@@ -64,34 +78,29 @@ BEGIN
 		FOREIGN KEY(Id_Funcionalidad) REFERENCES SIEGFRIED.FUNCIONALIDADES(Id_Funcionalidad)
 	);
 
-	CREATE TABLE SIEGFRIED.ESTADOS_CIVILES( 
-		id_estado numeric(18,0) identity(1,1) NOT NULL PRIMARY KEY,
-		descripcion nvarchar(255)
-	);
-
-	CREATE TABLE SIEGFRIED.AFILIADOS(
-		id_afiliado numeric(18,0) NOT NULL,
-		nombre nvarchar(255),
-		direccion nvarchar(255),
-		telefono numeric(18,0),
-		mail nvarchar(255),
-		fecha_nacimiento datetime,
-		sexo numeric(18,0) DEFAULT NULL,
-		estado_civil numeric(18,0) FOREIGN KEY REFERENCES SIEGFRIED.ESTADOS_CIVILES(id_estado),
-		cantidad_familiares numeric(18,0) DEFAULT 0,
-		id_plan numeric(18,0) foreign key references SIEGFRIED.PLANES(id_plan)
-
-		PRIMARY KEY(id_afiliado)
-	);
-
 	CREATE TABLE SIEGFRIED.PLANES(
 		id_plan numeric(18,0) PRIMARY KEY,
 		descripcion varchar(255),
 		precio_bono_consulta numeric(18,0)
 	);
 
+	CREATE TABLE SIEGFRIED.AFILIADOS(
+		id_afiliado numeric(18,0) NOT NULL,
+		estado_civil numeric(18,0) FOREIGN KEY REFERENCES SIEGFRIED.ESTADOS_CIVILES(id_estado),
+		cantidad_familiares numeric(18,0) DEFAULT 0,
+		id_plan numeric(18,0) foreign key references SIEGFRIED.PLANES(id_plan),
+		PRIMARY KEY(id_afiliado)
+	);
+
+		|				   
 	CREATE TABLE SIEGFRIED.PROFESIONALES ( 
-		id_profesional numeric(18,0) -- cuenta como matricula?
+		id_profesional numeric(18,0) NOT NULL PRIMARY KEY,
+		matricula varchar(255)--ppor las dudas dejo esto -- cuenta como matricula?
+	);
+
+	CREATE TABLE SIEGFRIED.TIPOS_ESPECIALIDADES ( 
+		id_tipo numeric(18,0) primary key,
+		descripcion varchar(255)
 	);
 
 	CREATE TABLE SIEGFRIED.ESPECIALIDADES (
@@ -100,10 +109,14 @@ BEGIN
 		id_tipo_especialidad numeric(18,0) foreign key references SIEGFRIED.TIPOS_ESPECIALIDADES(id_tipo)
 	);
 
-	CREATE TABLE SIEGFRIED.TIPOS_ESPECIALIDADES ( 
-		id_tipo numeric(18,0) primary key,
-		descripcion varchar(255)
-	);
+	CREATE TABLE SIEGFRIED.PROFESIONAL_ESPECIALIDAD ( 
+		Id_profesional			numeric(18,0),
+		Id_especialidad			numeric(18,0),
+		PRIMARY KEY(Id_profesional,Id_especialidad),
+		FOREIGN KEY(Id_profesional) REFERENCES SIEGFRIED.PROFESIONALES(Id_profesional),
+		FOREIGN KEY(Id_especialidad) REFERENCES SIEGFRIED.ESPECIALIDADES(Id_especialidad)
+	);	
+
 
 	CREATE TABLE SIEGFRIED.AGENDA (
 		id_profesional numeric(18,0) not null,
@@ -139,16 +152,18 @@ BEGIN
 		fecha_compra datetime
 	);
 
-	CREATE TABLE SIEGFRIED.CANCELACION(
-		id_turno numeric(18,0) foreign key references SIEGFRIED.TURNOS(id_turno),
-		id_tipo_cancelacion numeric(18,0) foreign key references SIEGFRIED.TIPO_CANCELACION(id_tipo),
-		primary key(id_turno, id_tipo_cancelacion)
-	);
-
 	CREATE TABLE SIEGFRIED.TIPOS_CANCELACION(
 		id_tipo numeric(18,0) not null identity(1,1) primary key,
 		descripcion varchar(255)
 	);
+
+	CREATE TABLE SIEGFRIED.CANCELACION(
+		id_turno numeric(18,0) foreign key references SIEGFRIED.TURNOS(id_turno),
+		id_tipo_cancelacion numeric(18,0) foreign key references SIEGFRIED.TIPOS_CANCELACION(id_tipo),
+		primary key(id_turno, id_tipo_cancelacion)
+	);
+
+	
     
 END
 GO
@@ -157,6 +172,56 @@ GO
 ---------------------------------------------------- MAIN EXECUTION ---------------------------------------------------
 BEGIN TRY
 	BEGIN TRANSACTION MAIN_T
+		INSERT INTO SIEGFRIED.USUARIOS
+		SELECT DISTINCT
+			Paciente_Nombre+Paciente_Apellido,   --username NVARCHAR(255),
+			'1234', --contrasenia binary(32),
+			1,	--habilitado		numeric(18,0) DEFAULT 1,
+			0,--cantlog			numeric(18,0) DEFAULT 0,
+			Paciente_Nombre, --nombre nvarchar(255),
+			Paciente_Apellido,
+			Paciente_Direccion, --direccion nvarchar(255),
+			'dni', --tipo_dni nvarchar(255),
+			Paciente_Dni, --nro_dni numeric(18,0),
+			Paciente_Telefono, --telefono numeric(18,0),
+			Paciente_Mail, --mail nvarchar(255),
+			Paciente_Fecha_Nac, --fecha_nacimiento datetime,
+			NULL
+		FROM gd_esquema.Maestra 
+		WHERE Paciente_Nombre IS NOT NULL;
+		
+		INSERT INTO SIEGFRIED.USUARIOS
+		SELECT DISTINCT
+			Medico_Nombre+Medico_Apellido,   --username NVARCHAR(255),
+			'1234', --contrasenia binary(32),
+			1,	--habilitado		numeric(18,0) DEFAULT 1,
+			0,--cantlog			numeric(18,0) DEFAULT 0,
+			Medico_Nombre, --nombre nvarchar(255),
+			Medico_Apellido,
+			Medico_Direccion, --direccion nvarchar(255),
+			'dni', --tipo_dni nvarchar(255),
+			Medico_Dni, --nro_dni numeric(18,0),
+			Medico_Telefono, --telefono numeric(18,0),
+			Medico_Mail, --mail nvarchar(255),
+			Medico_Fecha_Nac, --fecha_nacimiento datetime,
+			NULL
+		FROM gd_esquema.Maestra 
+		WHERE Paciente_Nombre IS NOT NULL;
+		
+		INSERT INTO SIEGFRIED.ESPECIALIDADES
+		SELECT DISTINCT
+			Especialidad_Codigo,
+			Especialidad_Descripcion
+		FROM gd_esquema.Maestra 
+		WHERE Especialidad_Codigo IS NOT NULL;
+
+		INSERT INTO SIEGFRIED.TIPOS_ESPECIALIDADES
+		SELECT DISTINCT
+			Tipo_Especialidad_Codigo,
+			Tipo_Especialidad_Descripcion
+		FROM gd_esquema.Maestra 
+		WHERE Tipo_Especialidad_Codigo IS NOT NULL;
+
 	--	HACER TODA LA NORMALIZACION DE LA TABLA MAESTRA ACA!!!
 	COMMIT TRANSACTION MAIN_T
 END TRY
